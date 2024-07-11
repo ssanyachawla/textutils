3d2814f1ce5dc4151e2fcf46ac091e97045cc57f
Sanya <chawlasanya16@gmail.com>
2024-07-11 20:01:10 +0530

    Updates

---
+++ precache-manifest.b37dc7e4d38957e77f2513fd53f6d7b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0174841252d3fd62ca741e9721ddc221",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "8088cbad7eeaa2dc60c2",
+    "url": "/textutils/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "9cc2427b0f6cc2c64d4e",
+    "url": "/textutils/static/css/main.a5394d52.chunk.css"
+  },
+  {
+    "revision": "8088cbad7eeaa2dc60c2",
+    "url": "/textutils/static/js/2.6080ed1a.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/textutils/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "9cc2427b0f6cc2c64d4e",
+    "url": "/textutils/static/js/main.544b9f9c.chunk.js"
+  },
+  {
+    "revision": "d657eb7742fad641386e",
+    "url": "/textutils/static/js/runtime~main.2c91e8f5.js"
+  }
+]);
